95700fd27330cd49b5e5a86145d3d47567496c5d
wanazi <903843602@qq.com>
2019-05-18 14:51:14 +0800

    加入-cargo

--- lifetime-tests/target/debug/deps/lifetime_tests-f34dbcdf89016da5.d
+++
@@ -1,5 +0,0 @@
-E:\workspaces\rust-eclipse\lifetime-tests\target\debug\deps\lifetime_tests-f34dbcdf89016da5.exe: src\main.rs
-
-E:\workspaces\rust-eclipse\lifetime-tests\target\debug\deps\lifetime_tests-f34dbcdf89016da5.d: src\main.rs
-
-src\main.rs:
