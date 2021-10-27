6b58fd1d64b5f4538831a1888b3ddde9e14f3b71
QidongTan <QidongTan@qq.com>
2021-10-27 17:33:04 +0800

    fix dependent library

--- 2021/windows-api-test/src/main.rs
+++ 2021/windows-api-test/src/main.rs
@@ -1,4 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use windows::UI::Color;
+use windows::Win32::UI::WindowsAndMessaging::*;
 
+fn main() {
+   let color=Color{
+       A:1,
+       R:3,
+       G:2,
+       B:1,
+   };
+   println!("{:?}",color);
+   unsafe{
+      MessageBoxA(None, "Text", "Caption", MB_OK);
+   }
+   
 }
